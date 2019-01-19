0434b6ad6f72eec29a808c4bdde5bc42615def32
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-18 20:06:02 -0500

    Create JS_For_Beginners.js

---
+++ JS_For_Beginners.js
@@ -0,0 +1,14 @@
+
+/*
+*
+* PRINT STATEMENT
+* ---------------
+* Console is the global object.
+* log() is a method of that object.
+* The log() method takes in a string as an argument.
+* It prints the output (basically, the input string) on the console.
+* Note: Console window prints different data types in different colors. 
+* For example, numbers are printed in 'green' and the Strings are printed in 'black',
+*/
+
+console.log("Hello World!");
