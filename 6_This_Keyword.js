fc38cc1aaf44b8b5293dabbd84462ef5fdd8258a
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-20 22:46:02 -0500

    Update 6_This_Keyword.js
    Accessor Methods in JS

--- 6_This_Keyword.js
+++ 6_This_Keyword.js
@@ -4,5 +4,40 @@
 * --------------------------------
 */
 
+/*
+*
+* How can one have accessor methods (Getters and Setters) in a language like JS?
+* 
+*/
 
+var person = {
+  "firstName": "Nirmal",
+  "lastName": "Kanagasabai",
+  "getFullName": function() {
+    return "Not implemented yet";
+  }
+};
 
+var fullName = person.getFullName();
+
+console.log(fullName);
+
+/*
+* output: Not implemented yet
+*/
+
+var employee = {
+  "firstName": "Nirmal",
+  "lastName": "Kanagasabai",
+  "getEmployeeName": function() {
+    return employee.firstName + " " + employee.lastName;
+  }
+};
+
+var employeeName = employee.getEmployeeName();
+
+console.log(employeeName);
+
+/*
+* output: Nirmal Kanagasabai
+*/
