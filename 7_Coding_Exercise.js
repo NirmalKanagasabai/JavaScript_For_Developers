845929d4049081b4e97c30a9d7891ed7c766002b
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-21 14:57:56 -0500

    Create 7_Coding_Exercise.js
    Small coding exercise

---
+++ 7_Coding_Exercise.js
@@ -0,0 +1,13 @@
+/*
+* --------------------------------
+* |  THIS KEYWORD IN JAVASCRIPT  |
+* --------------------------------
+*/
+
+var employee = {
+  "firstName": "Nirmal",
+  "lastName": "Kanagasabai",
+  "getEmployeeName": function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
