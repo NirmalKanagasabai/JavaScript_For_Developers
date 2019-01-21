b86292e8515b179d8c7c347631b2bf5f242dc739
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-21 15:43:27 -0500

    Update 7_Coding_Exercise.js

--- 7_Coding_Exercise.js
+++ 7_Coding_Exercise.js
@@ -1,7 +1,7 @@
 /*
-* --------------------------------
-* |  THIS KEYWORD IN JAVASCRIPT  |
-* --------------------------------
+* -----------------------------------
+* |  CODING EXERCISE IN JAVASCRIPT  |
+* -----------------------------------
 */
 
 var employee = {
@@ -9,5 +9,47 @@ var employee = {
   "lastName": "Kanagasabai",
   "getEmployeeName": function() {
     return this.firstName + " " + this.lastName;
+  },
+  
+  "address": {
+    "street": "123 JS Street",
+    "city": "JS",
+    "state": "CA"
+  },
+  "isFromState": function(state) {
+    if(this.address.state === state) {
+      return true;
+    } else {
+      return false;
+    }
   }
 };
+
+console.log(employee.isFromState("CA"));
+
+console.log(employee.isFromState("ABC"));
+
+/*
+* Optimized Version
+*/
+
+var employee = {
+  "firstName": "Nirmal",
+  "lastName": "Kanagasabai",
+  "getEmployeeName": function() {
+    return this.firstName + " " + this.lastName;
+  },
+  
+  "address": {
+    "street": "123 JS Street",
+    "city": "JS",
+    "state": "CA"
+  },
+  "isFromState": function(state) {
+    return this.address.state === state
+  }
+};
+
+console.log(employee.isFromState("CA"));
+
+console.log(employee.isFromState("ABC"));
