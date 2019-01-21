d80ae3fb7990e5492b9810f44f02775957359402
Nirmal Kanagasabai <nirmal.kanagasabai@gmail.com>
2019-01-20 19:06:45 -0500

    Create 5_Functions.js
    Separate JS file for Functions :)

---
+++ 5_Functions.js
@@ -0,0 +1,28 @@
+/*
+* ---------------------------
+* |  FUNCTIONS IN JAVASCRIPT  |
+* ---------------------------
+*/
+
+/*
+* 
+* Traditionally, the concept is very similar to other programming languages. 
+* 
+*/
+
+/*
+* Function Declaration. 
+*/
+function sayHello() {
+  console.log("Hello");
+}
+
+/*
+* Function call is made here. 
+*/
+sayHello();
+
+/*
+* output: Hello  
+*/
+
